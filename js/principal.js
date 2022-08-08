325ba73ba6c2824f5bb0adb0feebd1210e6b1ebc
Marcone Tenorio <marconetsf@gmail.com>
2022-08-08 10:25:04 -0300

    aula 4

--- js/principal.js
+++ js/principal.js
@@ -1,9 +1,44 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function () {
+    var form = document.querySelector("#form-adiciona");
+    var nome = form.querySelector("#nome").value;
+    var peso = form.querySelector("#peso").value;
+    var altura = form.querySelector("#altura").value;
+    var gordura = form.querySelector("#gordura").value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = function (peso, altura){
+        return peso / (altura * altura);
+    }.toFixed(2);
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+});
+
 var pacientes = document.querySelectorAll(".paciente");
 
-for(var i = 0; i < pacientes.length ; i++){
+for (var i = 0; i < pacientes.length; i++) {
     var paciente = pacientes[i];
     var tdPeso = paciente.querySelector(".info-peso");
     var tdAltura = paciente.querySelector(".info-altura");
@@ -16,22 +51,23 @@ for(var i = 0; i < pacientes.length ; i++){
     var alturaEhValida = true;
     var pesoEhValido = true;
 
-    if(peso <= 0 || peso >= 1000){
+    if (peso <= 0 || peso >= 1000) {
         console.log("Peso inválido");
         tdImc.textContent = "Peso inválido!";
         pesoEhValido = false;
         paciente.classList.add("paciente-invalido");
     }
 
-    if(altura <= 0 || altura >= 3.0){
+    if (altura <= 0 || altura >= 3.0) {
         console.log("Altura inválida");
         tdImc.textContent = "Altura inválida!";
         alturaEhValida = false;
         paciente.classList.add("paciente-invalido");
     }
 
-    if(pesoEhValido && alturaEhValida){
-        var imc = peso / ( altura * altura);    
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
         tdImc.textContent = imc.toFixed(2);
-    }    
+    }
 }
+
